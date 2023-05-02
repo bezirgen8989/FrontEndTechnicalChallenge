2b239a2c2f66bb19c32265ac618831f15b6cac06
Bezirgen Musayev <bezirgenmusayew@gmail.com>
2023-05-02 05:30:32 +0300

    fix for gh-pages 15

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   assetPrefix:"/FrontEndTechnicalChallenge/",
   reactStrictMode: true,
   publicRuntimeConfig: {
-    publicUrl: '/FrontEndTechnicalChallenge',
+    publicUrl: 'public/FrontEndTechnicalChallenge',
   },
   images: {
     loader: 'akamai',
