9bc9a35b0a267742591e425267611351b5aaefd5
Bezirgen Musayev <bezirgenmusayew@gmail.com>
2023-05-02 05:13:53 +0300

    fix for gh-pages 11

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const basePath = `/${repo}`
 
 const nextConfig = {
   basePath: "https://bezirgen8989.github.io/FrontEndTechnicalChallenge",
+  assetPrefix:"https://bezirgen8989.github.io/FrontEndTechnicalChallenge/",
   reactStrictMode: true,
   publicRuntimeConfig: {
     publicUrl: '/public',
