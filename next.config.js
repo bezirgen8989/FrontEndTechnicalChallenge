e5b090b5f773cda8ed080fc8c5622df8bbc258cf
Bezirgen Musayev <bezirgenmusayew@gmail.com>
2023-05-07 16:28:50 +0300

    fix api

--- next.config.js
+++ next.config.js
@@ -2,8 +2,14 @@
 const path = require("path");
 
 const nextConfig = {
-  // assetPrefix: "/FrontEndTechnicalChallenge/",
-  // basePath: "/FrontEndTechnicalChallenge",
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://bezirgen8989.github.io/api/:path*',
+      },
+    ]
+  },
   reactStrictMode: true,
   exportTrailingSlash: true,
   images: {
